b9d585d1ba37ec4c2c5471a296c24ae6cd00eeb8
Anuragh K P <kpanuragh@gmail.com>
2024-06-21 12:30:59 +0530

    Update plugin commits and switch to cyberdream theme
     This commit updates the commit hashes for various plugins to their latest versions, ensuring that we have the latest features
     and bug fixes. Additionally, the theme has been switched from 'nightfly' to 'cyberdream.nvim', with a configuration set for a
     transparent background. The 'lualine' plugin configuration has been updated to use the 'auto' theme setting.
    
     The 'noice.nvim' plugin has been added with a configuration that enhances the LSP messages and command line interface, providi
     a better user experience. It includes presets for common configurations and dependencies on 'nui.nvim' and 'nvim-notify' for U
     components.
    
     Other minor plugin updates and additions have been made to improve the overall functionality and aesthetics of the development
     environment.

--- lua/plugins/theming.lua
+++ lua/plugins/theming.lua
@@ -15,32 +15,63 @@ return {
 	-- 		require("nordic").load()
 	-- 	end,
 	-- },
-    { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
+	{
+		"scottmckendry/cyberdream.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("cyberdream").setup({
+				transparent = true,
+			})
+			vim.cmd("colorscheme cyberdream")
+		end,
+	},
+	--{ "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
 	{
 		"nvim-lualine/lualine.nvim",
 		config = function()
 			require("lualine").setup({
 				options = {
-					theme = "nightfly",
+					theme = "auto",
 				},
 			})
 		end,
 	},
-	-- {
-	-- 	"folke/noice.nvim",
-	-- 	event = "VeryLazy",
-	-- 	opts = {
-	-- 		-- add any options here
-	-- 	},
-	-- 	dependencies = {
-	-- 		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-	-- 		"MunifTanjim/nui.nvim",
-	-- 		-- OPTIONAL:
-	-- 		--   `nvim-notify` is only needed, if you want to use the notification view.
-	-- 		--   If not available, we use `mini` as the fallback
-	-- 		"rcarriga/nvim-notify",
-	-- 	},
-	-- },
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add any options here
+		},
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+		config = function()
+			require("noice").setup({
+				lsp = {
+					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+					},
+				},
+				-- you can enable a preset for easier configuration
+				presets = {
+					bottom_search = true, -- use a classic bottom cmdline for search
+					command_palette = true, -- position the cmdline and popupmenu together
+					long_message_to_split = true, -- long messages will be sent to a split
+					inc_rename = false, -- enables an input dialog for inc-rename.nvim
+					lsp_doc_border = false, -- add a border to hover docs and signature help
+				},
+			})
+		end,
+	},
 	-- setup rainbow-delimiters.nvim
 	{
 		"HiPhish/rainbow-delimiters.nvim",
@@ -53,7 +84,7 @@ return {
 			"nvim-tree/nvim-web-devicons",
 		},
 		config = function()
-            require("config.bufferline")
-        end,
+			require("config.bufferline")
+		end,
 	},
 }
